7faa991df6bb022d14c949de8eea28c176d85ebd
Gabriela Montagner Beccon <54218874808@>
2025-04-24 11:32:14 -0300

    fiz nada

--- front/app/src/components/sidebar/ItemSideBar.jsx
+++ front/app/src/components/sidebar/ItemSideBar.jsx
@@ -3,7 +3,7 @@ export default function ItemSideBar({icon, label, selected, onClick}){
         <div className={`flex gap-x-4`} onClick={()=>onClick()}>
             {icon}
 
-            <p className={`${selected ? 'text-[#9A95BA] font-medium ' : 'text-[#F4F4F4]'} text-xl`}>{label}</p>
+            <p className={`${selected ? 'text-[#9A95BA] font-[24px] ' : 'text-[#F4F4F4]'} text-xl`}>{label}</p>
         </div>
 
     )
--- front/app/src/components/sidebar/Sidebar.jsx
+++ front/app/src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import HomeIcon from "../../images/icons/HomeIcon"
 import ItemSideBar from "./ItemSideBar"
 import '../../index.css'
+import OsIcon from "../../images/icons/OsIcon"
 
 export default function SideBar(){
 
@@ -13,12 +14,14 @@ export default function SideBar(){
         setSelected(selected)
     }
 
+    const [teste, setTeste] = useState([1,2,3])
+    console.log(teste.length)
     return(
         <aside className={`${open ? ' w-72 ':'w-24'} bg-[#2D2D2D] h-screen`}>
 
             <div className="mx-10 py-10 flex flex-col gap-y-8">
                 <ItemSideBar icon={<HomeIcon selected={selected==='home'} />} selected={selected==='home'} label={'home'} onClick={()=>changeSelected('home')}/>
-                <ItemSideBar icon={<HomeIcon selected={selected==='os'} />} selected={selected==='os'} label={'os'} onClick={()=>changeSelected('os')}/>
+                <ItemSideBar icon={<OsIcon selected={selected==='os'} />} selected={selected==='os'} label={'os'} onClick={()=>changeSelected('os')}/>
                 <ItemSideBar icon={<HomeIcon selected={selected==='patrimonios'} />} selected={selected==='patrimonios'} label={'patrimonios'} onClick={()=>changeSelected('patrimonios')}/>
                 <ItemSideBar icon={<HomeIcon selected={selected==='gerentes'} />} selected={selected==='gerentes'} label={'gerentes'} onClick={()=>changeSelected('gerentes')}/>
                 <ItemSideBar icon={<HomeIcon selected={selected==='manutentores'} />} selected={selected==='manutentores'} label={'manutentores'} onClick={()=>changeSelected('manutentores')}/>
